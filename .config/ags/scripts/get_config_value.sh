c3b44d50993f3254908f90b60dd05a1e57fbcc87
Josef Ráž <josefraz@seznam.cz>
2025-11-18 15:45:56 +0100

    Fixed empty color after reloading with default config.

--- .config/ags/scripts/get_config_value.sh
+++ .config/ags/scripts/get_config_value.sh
@@ -5,12 +5,7 @@ MODE=$(darkman get)
 
 # Create ~/.config/sway/config.json with default background
 if [ ! -f ~/.config/sway/config.json ]; then
-    jq -n '{background: "~/.config/sway/backgrounds/LightWaves-b4b59bda185758ebaa2735e4e9fc78a2f7277c64.webp", mode: "'$MODE'", color: "'$COLOR'"}' > ~/.config/sway/config.json
-else
-    BACKGROUND_FILE=$(jq -r '.background' ~/.config/sway/config.json)
-    COLOR=$(~/.config/sway/scripts/get_color.py $BACKGROUND_FILE)
-    jq ".background = \"$BACKGROUND_FILE\" | .mode = \"$MODE\" | .color = \"$COLOR\"" ~/.config/sway/config.json > ~/.config/sway/config.json.tmp && \
-    mv ~/.config/sway/config.json.tmp ~/.config/sway/config.json
+    jq -n '{background: "", mode: "'$MODE'", color: "'$COLOR'"}' > ~/.config/sway/config.json
 fi
 
 # Check if property name argument is provided
--- .config/sway/scripts/exec_always.sh
+++ .config/sway/scripts/exec_always.sh
@@ -8,7 +8,9 @@ if [ ! -f ~/.config/sway/config.json ]; then
     jq -n '{background: "~/.config/sway/backgrounds/LightWaves-b4b59bda185758ebaa2735e4e9fc78a2f7277c64.webp", mode: "'$MODE'", color: "'$COLOR'"}' > ~/.config/sway/config.json
 else
     BACKGROUND_FILE=$(jq -r '.background' ~/.config/sway/config.json)
-    COLOR=$(~/.config/sway/scripts/get_color.py $BACKGROUND_FILE)
+    # Expand ~ to home directory
+    BACKGROUND_FILE_EXPANDED="${BACKGROUND_FILE/#\~/$HOME}"
+    COLOR=$(~/.config/sway/scripts/get_color.py $BACKGROUND_FILE_EXPANDED)
     jq ".background = \"$BACKGROUND_FILE\" | .mode = \"$MODE\" | .color = \"$COLOR\"" ~/.config/sway/config.json > ~/.config/sway/config.json.tmp && \
     mv ~/.config/sway/config.json.tmp ~/.config/sway/config.json
 fi
