3902299d8a7cf8e3facd547dd4a320cf805d7162
Josef Ráž <josefraz@seznam.cz>
2025-12-27 13:47:02 +0100

    Enhance tray icons integration and adjust UI component styles for improved visibility

--- .config/ags/components/bar/modules/trayIcons/TrayIcons.tsx
+++ .config/ags/components/bar/modules/trayIcons/TrayIcons.tsx
@@ -2,17 +2,10 @@ import { Gtk } from "ags/gtk4";
 import AstalTray from "gi://AstalTray";
 import { createBinding, For } from "gnim";
 
+// https://github.com/Aylur/ags/blob/main/examples/gtk4/simple-bar/Bar.tsx
 export default function TrayIcons() {
     const tray = AstalTray.get_default()
     const items = createBinding(tray, "items")
-
-    const init = (btn: Gtk.MenuButton, item: AstalTray.TrayItem) => {
-        btn.menuModel = item.menuModel
-        btn.insert_action_group("dbusmenu", item.actionGroup)
-        item.connect("notify::action-group", () => {
-            btn.insert_action_group("dbusmenu", item.actionGroup)
-        })
-    }
 
     return (
         <box
@@ -20,9 +13,14 @@ export default function TrayIcons() {
         >
             <For each={items}>
                 {(item) => (
-                    <menubutton $={(self) => init(self, item)}>
+                    <button
+                        class="transparentThenHoverFg"
+                        onClicked={() => {
+                            item.activate(0, 0);
+                        }}
+                    >
                         <image gicon={createBinding(item, "gicon")} />
-                    </menubutton>
+                    </button>
                 )}
             </For>
         </box>
